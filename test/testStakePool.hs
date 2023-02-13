e99b7c0efb72974c83d13e093fd0abaa5e44a9e4
calendfi <115602239+calendfi@users.noreply.github.com>
2023-02-13 15:27:47 +0800

    testStakePool

---
+++ test/testStakePool.hs
@@ -0,0 +1,17 @@
+/*Create an initial pool named pool.
+Call the stake function to mortgage 10 units of assets to the pool.
+Call the unstake function to withdraw 5 units of assets from the pool.
+Returns the updated pool and final total stake.
+Finally, we specify the desired outcome, i.e. the returned pool should be a pool containing the staker address and 5 units of assets, for a total stake of 5 units.
+*/
+
+
+testStakeAndUnstake : TestCase
+testStakeAndUnstake = TestCase
+  { description = "Stake and unstake"
+  , program = do
+      let pool = initialState
+      pool' <- addStake 10 pool
+      pool'' <- removeStake 5 pool'
+      return (pool'', pool.totalStake + 10 - 5)
+  , expect = Just (Pool (Address 0) 5 [(getAddress(), 5)])
